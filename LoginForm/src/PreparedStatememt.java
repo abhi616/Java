bf86f8a1ec055fd1c09cd9ff12a56170f42c09cd
Abhishek Yadav <ay891241>
2021-12-03 22:48:42 +0530

    Login Form

---
+++ LoginForm/src/PreparedStatememt.java
@@ -0,0 +1,32 @@
+
+import javax.swing.JPasswordField;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abhishek
+ */
+class PreparedStatememt {
+
+    void setString(int i, String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setString(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setString(JPasswordField tb_Password, String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    ResultSet executeQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
